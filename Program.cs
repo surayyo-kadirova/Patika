e4d562de439bbb19258dd862f987af5569a3eb01
Surayyo <124129374+surayyo-kadirova@users.noreply.github.com>
2024-10-17 09:49:10 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,76 +1,79 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("*****Matematiksel İfadeler*****");
-//Matematiksel ifadeler
-int x = 3;
-int y = 3;
-y = y + 2;
+﻿using System;
 
-Console.WriteLine(y);
 
-y += 2;
-Console.WriteLine(y);
-
-y /= 1;
-Console.WriteLine(y);
-
-x += 2;
-Console.WriteLine(x);
-
-Console.WriteLine("*****Mantıksal İfadeler*****");
-//Mantıksal İfadeler
-bool isSuccess = true;
-bool isCompleted = false;
-
-if(isSuccess && isCompleted)
+namespace Patika
 {
-    Console.WriteLine("Perfect");
+    class Program{
+
+        static void Main(string[] args)
+        {
+            byte b = 5;  //1byte
+            sbyte c = 5; //1byte
+
+            short s = 5;    //2 byte
+            ushort us = 5;  //2 byte
+
+            Int16 int16= 2; //2 byte
+            int i = 2;      //4 byte
+            Int32 int32 = 3;  //4 byte
+            Int64 int64 = 4;  //8 byte
+            uint ui = 2;      //4 byte
+
+            long l = 4;      //8 byte
+            ulong ul = 4;    //8 byte
+
+            // real sayılar
+            float fl = 4;    //4 byte
+            double d = 4;   //8 byte
+            decimal dc = 2;  //16 byte
+
+            char cr = 'c';   //2 byte
+            string st = "Süreyya";   //sınırsız
+
+            bool t = true;
+            bool f = false;
+
+            DateTime dt = DateTime.Now;
+            Console.WriteLine(dt);
+
+            object o1 = "x";
+            object o2 = "y";
+            object o3 = 4;
+            object o4 = 4.3;
+
+            string str = String.Empty;
+            string str1 = null;
+            str1 = "Merhaba";
+            string ad = "Süreyya";
+            string soyad = "Kadirova";
+            string tamisim = ad + "" + soyad;
+
+            int integer1= 5;
+            int integer2 = 3;
+            int integer3 = integer1 * integer2;
+
+            bool b1 = 10 < 2;
+            bool b2 = 10 > 2;
+
+            //Değiişken dönüşümleri
+            string str5 = "20";
+            int i20 = 20;
+            string yenideger = str5 + i20.ToString();
+            Console.WriteLine(yenideger);
+
+            string str2 = "2100";
+            int i202 = 399;
+            string yenideger1 = str5 + i20.ToString();
+            Console.WriteLine(yenideger1);
+
+
+
+
+
+        }
+
+    }
 }
 
-if(isSuccess || isCompleted)
-{
-    Console.WriteLine("Great");
-}
-
-if(isSuccess && !isCompleted)
-{
-    Console.WriteLine("Fine");
-}
-Console.WriteLine("*****İlişkisel Operatotler*****");
-//İlişkisel ifadeler
-int a = 3;
-int b = 4;
-bool sonuc = a<b;
-
-Console.WriteLine(sonuc);
-sonuc = a > b;
-Console.WriteLine(sonuc);
-sonuc = a>=b;
-Console.WriteLine(sonuc);
-sonuc = a <=b;
-Console.WriteLine(sonuc);
-sonuc = a==b;
-Console.WriteLine(sonuc);
-sonuc = a!=b;
-Console.WriteLine(sonuc);
-
-Console.WriteLine("*****Aritmetik Operatorler*****");
-//Aritmetik ifadeler
-
-int sayi1 = 10;
-int sayi2 = 5;
-int sonuc1 = sayi1 / sayi2;
-Console.WriteLine(sonuc1);
-
-sonuc1 = sayi1 * sayi2;
-Console.WriteLine(sonuc1);
-sonuc1 = sayi1 + sayi2;
-Console.WriteLine(sonuc1);
-sonuc1 = sayi1+=1;
-Console.WriteLine(sonuc1);
-
-// % Mod alır
-
-int sonuc2 = 20%3;
-Console.WriteLine(sonuc2);
 
 
